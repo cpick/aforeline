517b307bcf8519301cc7c4aa2bf7b8d0f90168b6
Chris Pick <chris@chrispick.com>
2015-11-06 17:45:16 -0500

    Have vagrant build package automatically when provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,6 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.synced_folder ".", "/vagrant"
+
+  config.vm.provision "shell", path: "build"
 end
---
+++ package_vars.sh
@@ -0,0 +1,7 @@
+# intended to be sourced by other /bin/sh scripts
+
+PACKAGE_NAME="`dpkg-parsechangelog --show-field Source`"
+PACKAGE_VERSION="`dpkg-parsechangelog --show-field Version`"
+PACKAGE_DIR="${PACKAGE_NAME}-${PACKAGE_VERSION}"
+PACKAGE_FILE_PREFIX="${PACKAGE_NAME}_${PACKAGE_VERSION}"
+CHANGES_FILE="${PACKAGE_FILE_PREFIX}_source.changes"
