34d7c691d18bd1d8edcbf2832fd1b1bf61d3ab92
Chris Pick <chris@chrispick.com>
2018-08-10 17:27:40 -0400

    Support bionic
    Make changes required to support Ubuntu bionic.

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/bionic64"
   config.vm.synced_folder ".", "/vagrant"
 
   config.vm.provision "shell", path: "build"
--- package_vars.sh
+++ package_vars.sh
@@ -2,6 +2,6 @@
 
 PACKAGE_NAME="`dpkg-parsechangelog --show-field Source`"
 PACKAGE_VERSION="`dpkg-parsechangelog --show-field Version`"
-PACKAGE_DIR="${PACKAGE_NAME}-${PACKAGE_VERSION}"
+PACKAGE_DIR="${PACKAGE_NAME}-${PACKAGE_VERSION%-*}"
 PACKAGE_FILE_PREFIX="${PACKAGE_NAME}_${PACKAGE_VERSION}"
 CHANGES_FILE="${PACKAGE_FILE_PREFIX}_source.changes"
